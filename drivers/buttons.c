a20083b9b5f4080106294c275aa1703211f255ff
hesham-akmal <hesham.akmal@gmail.com>
2018-05-01 19:59:16 +0200

    more abstract implementations
    press button > led turns on + PA2 output

--- drivers/buttons.c
+++ drivers/buttons.c
@@ -46,66 +46,84 @@
 ////////////////////////////
 
 #define SWITCHTASKSTACKSIZE        128 
+/*
+////////////////////Central Buttons Port and Pins
+#define CentralBTNS_SYSCTL_PERIPH_GPIO SYSCTL_PERIPH_GPIOA
+#define CentralBTNS_GPIO_PORT_BASE GPIO_PORTA_BASE 
+#define CentralBtn1pin GPIO_PIN_6
+#define CentralBtn2pin GPIO_PIN_7
+//////////////////////*/
 
-extern xSemaphoreHandle xButtonPressedSemaphore;
+//trying with PF0 and PF4 for Central Buttons
+#define CentralBTNS_GPIO_PORT_BASE GPIO_PORTF_BASE 
+#define CentralBtn1pin GPIO_PIN_0
+#define CentralBtn2pin GPIO_PIN_4
 
-//*****************************************************************************
-//
-//! \addtogroup buttons_api
-//! @{
-//
-//*****************************************************************************
 
-extern xQueueHandle g_pLEDQueue;
+////////////////////Motor Port and Pins
+#define Motor_SYSCTL_PERIPH_GPIO SYSCTL_PERIPH_GPIOA
+#define Motor_GPIO_PORT_BASE GPIO_PORTA_BASE 
+#define MotorPin GPIO_PIN_2
+//////////////////////
 
-bool PIN_0_Pressed = false;
-bool PIN_4_Pressed = false;
+
+volatile bool CentralBtn1pin_Pressed = false;
+volatile bool CentralBtn2pin_Pressed = false;
+
+//extern xSemaphoreHandle xButtonPressedSemaphore;
+//extern xQueueHandle g_pLEDQueue;
 
 void onButtonInt(void) {
 
 		//Get which pin interrupted
-		uint8_t PIN_NUM = GPIOIntStatus(GPIO_PORTF_BASE, false);
+		uint8_t INT_PIN_NUM = GPIOIntStatus(CentralBTNS_GPIO_PORT_BASE, false);
 
-		if (PIN_NUM & GPIO_PIN_0) //PF0 btn CLICK //if PIN_NUM == GPIO_PIN_0
+		if (INT_PIN_NUM & CentralBtn1pin) //if INT_PIN_NUM == CentralBtn1pin
 		{
-			if (!PIN_0_Pressed) //If btn not held down
+		
+			if (!CentralBtn1pin_Pressed) //If btn not held down ( Btn pressed )
 			{
-				 UARTprintf("PF0 Down \n");
+				 UARTprintf("CentralBtn1 Down \n");
 				 //turn on led
 				 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 , 2);
 				 //turn on motor
-				 GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, GPIO_PIN_2);
+				 GPIOPinWrite(Motor_GPIO_PORT_BASE, MotorPin, MotorPin);
 			}
-			else // btn was held down
+			else // btn was held down ( Btn released )
 			{
-					UARTprintf("PF0 Up \n");
+					UARTprintf("CentralBtn1 Up \n");
 					//turn off led
 					GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0 );
-					//turn off motor?
-					GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, 0);
+					//turn off motor
+				  GPIOPinWrite(Motor_GPIO_PORT_BASE, MotorPin, 0);
       }
-				PIN_0_Pressed = !PIN_0_Pressed; //flip pressed bool 
-				GPIOIntClear(GPIO_PORTF_BASE, PIN_NUM);  // Clear interrupt flag
+				CentralBtn1pin_Pressed = !CentralBtn1pin_Pressed; //flip pressed bool 
+				GPIOIntClear(CentralBTNS_GPIO_PORT_BASE, INT_PIN_NUM);  // Clear interrupt flag
+				
 		}
-		
-		else if (PIN_NUM & GPIO_PIN_4) //PF4 btn CLICK
+		else if (INT_PIN_NUM & CentralBtn2pin)
 		{
-			if (!PIN_4_Pressed)
+		
+			if (!CentralBtn2pin_Pressed)
 			{
-				 UARTprintf("PF4 Down \n");
+				 UARTprintf("CentralBtn2 Down \n");
 				 //turn on led
 				 GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 , 2);
+				 //turn on motor
+				 GPIOPinWrite(Motor_GPIO_PORT_BASE, MotorPin, MotorPin);
 			}
 			else
 			{
-				 UARTprintf("PF4 Up \n");
+				 UARTprintf("CentralBtn2 Up \n");
 				 //turn off led
 				 GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0 );
+				 //turn off motor
+				 GPIOPinWrite(Motor_GPIO_PORT_BASE, MotorPin, 0);
 			}
-				 PIN_4_Pressed = !PIN_4_Pressed;
-				 GPIOIntClear(GPIO_PORTF_BASE, PIN_NUM);  // Clear interrupt flag
+				 CentralBtn2pin_Pressed = !CentralBtn2pin_Pressed;
+				 GPIOIntClear(CentralBTNS_GPIO_PORT_BASE, INT_PIN_NUM);  // Clear interrupt flag
+				 
 		}
-		
 }
 
 //*****************************************************************************
@@ -145,25 +163,25 @@ ButtonsInit(void)
     MAP_GPIOPadConfigSet(BUTTONS_GPIO_BASE, ALL_BUTTONS,
                          GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
-		//DISABLE INTERRUPTS AT INIT
-		GPIOIntDisable(GPIO_PORTF_BASE, GPIO_PIN_0);
-		GPIOIntDisable(GPIO_PORTF_BASE, GPIO_PIN_4);
-		// To set interrupts 
-    GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_BOTH_EDGES);
-		GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_BOTH_EDGES);
-		// ENABLE INTERRUPTS
-		GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_0);
-		GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_4);
-
-		//Set Handler to onButtonInt method
-		GPIOIntRegister(GPIO_PORTF_BASE, onButtonInt);
-		
 		//enable led for testing
 		GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
 		
+		//enable entral Buttons pins, CentralBTNS_GPIO_PORT_BASE and pin numbers are defined at the start of this doc ////////////////////////////////////
+    //ROM_SysCtlPeripheralEnable(CentralBTNS_SYSCTL_PERIPH_GPIO); //comment this line out if you're trying PF0 and PF4
+		//GPIOPinTypeGPIOInput(CentralBTNS_GPIO_PORT_BASE, CentralBtn1pin|CentralBtn2pin); //comment this line out if you're trying PF0 and PF4
+		//Central Buttons INTERRUPT INIT //////////////////////////////////
+		GPIOIntDisable(CentralBTNS_GPIO_PORT_BASE, CentralBtn1pin);
+		GPIOIntDisable(CentralBTNS_GPIO_PORT_BASE, CentralBtn2pin);
+    GPIOIntTypeSet(CentralBTNS_GPIO_PORT_BASE, CentralBtn1pin, GPIO_BOTH_EDGES);
+		GPIOIntTypeSet(CentralBTNS_GPIO_PORT_BASE, CentralBtn2pin, GPIO_BOTH_EDGES);
+		GPIOIntEnable(CentralBTNS_GPIO_PORT_BASE, CentralBtn1pin);
+		GPIOIntEnable(CentralBTNS_GPIO_PORT_BASE, CentralBtn2pin);
+		////////////////////////////////////////////////////////////////////
+		GPIOIntRegister(CentralBTNS_GPIO_PORT_BASE, onButtonInt);
+		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
 		//enable motor pins
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-		GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_2);
+		GPIOPinTypeGPIOOutput(Motor_GPIO_PORT_BASE, MotorPin);
 }
 //*****************************************************************************
 //
